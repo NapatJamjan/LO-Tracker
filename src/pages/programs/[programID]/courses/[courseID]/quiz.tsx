a60245acf58fb3189ecc833cf20b12ef999df228
Nuttawut503 <nuttawut.promsuk@gmail.com>
2021-10-02 10:46:41 +0700

    updated quiz page

--- src/pages/programs/[programID]/courses/[courseID]/quiz.tsx
+++ src/pages/programs/[programID]/courses/[courseID]/quiz.tsx
@@ -9,11 +9,16 @@ import xlsx from 'xlsx';
 
 interface QuizResponse {
   quizID: string;
-  title: string;
+  quizName: string;
   questions: {
     questionID: string;
-    title: string;
+    questionTitle: string;
     maxScore: number;
+    linkedLOs: {
+      loID: string;
+      level: number;
+      levelDescription: string;
+    }[]
   }[]
 }
 
@@ -21,7 +26,7 @@ interface QuestionUpload {
   /**
    * Excel Header Format
    */
-  title: string;
+  questionTitle: string;
   maxScore: number;
   studentID: string;
   studentScore: number;
@@ -34,7 +39,7 @@ const Quiz: React.FC<{programID: string, courseID: string}> = ({programID, cours
       baseURL: 'http://localhost:5000/api'
     });
     api
-      .get<QuizResponse[]>('/quizzes', { params: { courseID } })
+      .get<QuizResponse[]>('/quizzes', { params: { programID, courseID } })
       .then((res) => res.data)
       .then(setQuizzes);
   }, []);
@@ -43,7 +48,7 @@ const Quiz: React.FC<{programID: string, courseID: string}> = ({programID, cours
       baseURL: 'http://localhost:5000/api'
     });
     api
-      .post<QuestionUpload[]>('/questions', { quizID, questions: JSON.stringify(questions) })
+      .post<QuestionUpload[]>('/questions', { programID, courseID, quizID, questions: JSON.stringify(questions) })
       .then(() => {
         window.location.reload();
       });
@@ -74,13 +79,25 @@ const Quiz: React.FC<{programID: string, courseID: string}> = ({programID, cours
       </p>
       {quizzes.map((quiz) => (
         <div key={quiz.quizID} className="rounded shadow-lg p-3">
-          {quiz.title}
+          {quiz.quizName}
           <ul>
           {
             quiz.questions && quiz.questions.map((question, index) => (
               <div key={question.questionID} style={{borderBottom:"grey 1px solid"}}>
-              <li key={question.questionID}>Q {index + 1} - {question.title} (max score: {question.maxScore})</li>
-              <CreateQuestionLinkForm courseID={courseID} question={question.title}/>
+              <li key={question.questionID}>
+                <span>Q {index + 1} - {question.questionTitle} (max score: {question.maxScore})</span>
+                {JSON.stringify(question)}
+                <ol>
+                  {
+                    question.linkedLOs.map((lvl) => (
+                      <li>
+                        <span>Level {lvl.level} - {lvl.levelDescription}</span>
+                      </li>
+                    ))
+                  }
+                </ol>
+              </li>
+              <CreateQuestionLinkForm programID={programID} courseID={courseID} quizID={quiz.quizID} questionID={question.questionID}/>
               </div>
             ))
           }
@@ -88,26 +105,26 @@ const Quiz: React.FC<{programID: string, courseID: string}> = ({programID, cours
           <input type="file" onChange={(e) => excelJSON(e.target.files[0], quiz.quizID)} />
         </div>
       ))}
-      <CreateQuizForm courseID={courseID}/>
+      <CreateQuizForm programID={programID} courseID={courseID}/>
     </Layout>
   );
 }
 
-const CreateQuizForm: React.FC<{courseID: string}> = ({courseID}) => {
+const CreateQuizForm: React.FC<{programID: string, courseID: string}> = ({programID, courseID}) => {
   const [show, setShow] = useState<boolean>(false);
-  const { register, handleSubmit, setValue } = useForm<{ title: string }>();
+  const { register, handleSubmit, setValue } = useForm<{ quizName: string }>();
   return (
     <div>
       <button onClick={() => setShow(true)}>Create a new quiz.</button>
       <Modal show={show} onHide={() => setShow(false)}>
         <form
           onSubmit={handleSubmit((form) => {
-            if (form.title != '') {
+            if (form.quizName !== '') {
               const api = axios.create({
                 baseURL: 'http://localhost:5000/api'
               });
-              api.post('/quiz', {...form, courseID}).then(() => {
-                setValue('title', '');
+              api.post('/quiz', {...form, programID, courseID}).then(() => {
+                setValue('quizName', '');
                 setShow(false);
                 window.location.reload();
               });
@@ -118,7 +135,7 @@ const CreateQuizForm: React.FC<{courseID: string}> = ({courseID}) => {
             <Modal.Title>Create a new quiz</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <input type="text" {...register('title')} />
+            <input type="text" {...register('quizName')} />
           </Modal.Body>
           <Modal.Footer>
             <input type="submit" value="save" />
@@ -131,32 +148,31 @@ const CreateQuizForm: React.FC<{courseID: string}> = ({courseID}) => {
 
 interface LOResponse {
   loID: string;
-  info: string;
-  levels: Array<{
+  loTitle: string;
+  levels: {
     level: number;
-    info: string;
-  }>
+    levelDescription: string;
+  }[]
 }
 interface questionlinkRequest {
   questionID: string;
   loID: string;
   level: string;
 }
-const CreateQuestionLinkForm: React.FC<{courseID: string, question: string}> = ({courseID, question}) => {
+const CreateQuestionLinkForm: React.FC<{programID: string, courseID: string, quizID: string, questionID: string}> = ({programID, courseID, quizID, questionID}) => {
   const [show, setShow] = useState<boolean>(false);
-  const { register, handleSubmit, setValue } = useForm<{ title: string }>();
+  const { register, handleSubmit, setValue } = useForm<{ loIndex: number, levelIndex: number }>({defaultValues: {loIndex: 0, levelIndex: 0}});
   const [los, setLOs] = useState<LOResponse[]>([]);
+  const [selectedLOIndex, setSelectedLOIndex] = useState<number>(0);
   useEffect(() => {
     if (show) {
       const api = axios.create({
         baseURL: 'http://localhost:5000/api'
       });
       api
-        .get<LOResponse[]>('/los', { params: { courseID } })
+        .get<LOResponse[]>('/los', { params: { programID, courseID } })
         .then((res) => res.data)
         .then(setLOs);
-    } else {
-      //setValue('ploID', '');
     }
   }, [show])
   return (
@@ -165,33 +181,39 @@ const CreateQuestionLinkForm: React.FC<{courseID: string, question: string}> = (
       <Modal show={show} onHide={() => setShow(false)}>
         <form
           onSubmit={handleSubmit((form) => {
-            if (form.title != '') {
-              const api = axios.create({
-                baseURL: 'http://localhost:5000/api'
-              });
-              api.post('/quiz', {...form, courseID}).then(() => {
-                setValue('title', '');
-                setShow(false);
-                window.location.reload();
-              });
-            }
+            const api = axios.create({
+              baseURL: 'http://localhost:5000/api'
+            });
+            console.log(JSON.stringify( {programID, courseID, quizID, questionID, loID: los[form.loIndex].loID, level: los[form.loIndex].levels[form.levelIndex].level}))
+            api.post('/questionlink', {programID, courseID, quizID, questionID, loID: los[form.loIndex].loID, level: los[form.loIndex].levels[form.levelIndex].level}).then(() => {
+              setValue('loIndex', 0);
+              setShow(false);
+              window.location.reload();
+            });
           })}
         >
           <Modal.Header>
             <Modal.Title>Link a Question to LO Level</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h5>Target : {question}</h5>
-            {los.map((lo) => (
-              <div style={{borderBottom: "grey 1px solid", marginBottom:5}}>
-                {lo.info}
-                {lo.levels.map((lvl) => (
-                  <div>
-                    <input type="checkbox"/>Level {lvl.level} {lvl.info}
-                  </div>
-                ))}
-              </div>
-            ))}
+            <span>LO:</span>
+            <br />
+            <select {...register('loIndex')} onChange={e => {setSelectedLOIndex(parseInt(e.target.value, 10)); setValue('levelIndex', 0)}}>
+              {los.map((lo, index) => (
+                <option value={index} key={lo.loID}>
+                  {lo.loTitle}
+                </option>
+              ))}
+            </select><br/>
+            <span>LO Level:</span>
+            <br />
+            <select {...register('levelIndex')}>
+              {los[selectedLOIndex] && los[selectedLOIndex].levels.map((lvl, index) => (
+                <option value={index} key={`${los[selectedLOIndex].loID}-${lvl.level}`}>
+                  {lvl.levelDescription}
+                </option>
+              ))}
+            </select>
           </Modal.Body>
           <Modal.Footer>
             <input type="submit" value="save" />
