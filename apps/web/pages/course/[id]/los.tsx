850857ab2964ee93aeb1e9c399f156df51d30535
Nuttawut503 <nuttawut.promsuk@gmail.com>
2021-10-28 14:17:18 +0700

    feat: update LOs and Quiz page

--- apps/web/pages/course/[id]/los.tsx
+++ apps/web/pages/course/[id]/los.tsx
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import client from '../../../apollo-client';
-import { gql } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { CourseStaticPaths } from '../../../utils/staticpaths';
 import ProgramAnchor from '../../../components/ProgramAnchor';
 import ClientOnly from '../../../components/ClientOnly';
 import { ParsedUrlQuery } from 'querystring';
+import { Modal } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 interface CourseModel {
   id: string;
@@ -16,6 +19,57 @@ interface CourseModel {
   year: number;
   ploGroupID: string;
   programID: string;
+};
+
+interface PLOModel {
+  id: string;
+  title: string;
+  description: string;
+  ploGroupID: string;
+}
+
+interface LOModel {
+  id: string;
+  title: string;
+  levels: {
+    level: number;
+    description: string;
+  }[];
+  ploLinks: {
+    id: string;
+    title: string;
+    description: string;
+    ploGroupID: string;
+  }[];
+};
+
+interface CreateLOModel {
+  title: string;
+  level: number;
+  description: string;
+};
+
+interface CreateLOLinkResponse {
+  loID: string;
+  ploID: string;
+};
+
+interface CreateLOLevelModel {
+  level: number;
+  description: string;
+};
+
+interface CreateLOLevelResponse {
+  id: string;
+};
+
+interface CreateLOResponse {
+  id: string;
+};
+
+interface DeleteLOLinkResponse {
+  loID: string;
+  ploID: string;
 };
 
 export default function Index({course}: {course: CourseModel}) {
@@ -36,10 +90,284 @@ export default function Index({course}: {course: CourseModel}) {
       {' '}&#12297;{' '}
       <span>LOs</span>
     </p>
-    {JSON.stringify(course)}
-    <p>Hello</p>
+    <ClientOnly>
+      <LO courseID={course.id} ploGroupID={course.ploGroupID}/>
+    </ClientOnly>
   </div>);
 };
+
+function LO({courseID, ploGroupID}: {courseID: string, ploGroupID: string}) {
+  const GET_LOS = gql`
+    query LOs($courseID: ID!) {
+      los(courseID: $courseID) {
+        id
+        title
+        levels {
+          level
+          description
+        }
+        ploLinks {
+          id
+          title
+          description
+          ploGroupID
+        }
+      }
+    }
+  `;
+  const DELETE_LOLINK = gql`
+    mutation DeleteLOLink($loID: ID!, $ploID: ID!) {
+      deleteLOLink(loID: $loID, ploID: $ploID) {
+        loID
+        ploID
+      }
+    }
+  `;
+  const { data, loading, refetch } = useQuery<{los: LOModel[]}, {courseID: string}>(GET_LOS, { variables: { courseID } });
+  const [deleteLOLink, {loading: submitting}] = useMutation<{deleteLOLink: DeleteLOLinkResponse}, {loID: string, ploID: string}>(DELETE_LOLINK);
+  const [selectedLOID, setSelectedLOID] = useState<string>('');
+  let los: LOModel[] = [];
+  if (data) {
+    los = [...data.los];
+  }
+  const deleteLinkedPLO = (ploID: string) => {
+    if (!submitting) {
+      deleteLOLink({
+        variables: {
+          loID: selectedLOID,
+          ploID
+        }
+      }).then(() => refetch());
+    }
+  }
+  return (<>
+    <CreateLOForm courseID={courseID} callback={refetch}/>
+    <div className="grid grid-cols-2 gap-x gap-x-6 mt-2">
+      <div className="flex flex-column space-y-2">
+        {loading && <p>Loading...</p>}
+        {los.sort((l1, l2) => l1.title.localeCompare(l2.title)).map((lo) => (
+        <div key={lo.id} className="rounded shadow-lg p-3">
+          {lo.title}
+          <ul>
+          {
+            [...lo.levels].sort((l1, l2) => l1.level - l2.level).map((level) => (
+              <li key={`${lo.id}-${level.level}`}>
+                Level {level.level}<br/>{level.description}
+              </li>
+            ))
+          }
+          </ul>
+          <br/>
+          <div className="flex flex-row-reverse space-x-2">
+          <button
+            onClick={() => setSelectedLOID(lo.id)}
+            className={`bg-gray-200 hover:bg-gray-400 py-1 px-2 rounded text-sm ${selectedLOID===lo.id?'bg-blue-400 hover:bg-blue-300':''}`}>
+            Manage PLOs <span className="text-xl text-green-800">&#9874;</span>
+          </button>
+            <span className="px-2"></span>
+            <CreateLOLevelForm loID={lo.id} initLevel={lo.levels.length + 1} callback={refetch}/>
+          </div>
+        </div>
+      ))}
+      </div>
+      <div>
+        {selectedLOID !== '' && 
+        <div className="flex flex-column divide-y-4">
+          <CreateLOLink loID={selectedLOID} ploGroupID={ploGroupID} callback={refetch}/>
+          <div className="pt-3">
+            <span>Linked PLOs: </span><br/>
+            <ul>
+            {
+              [...los[los.findIndex((lo) => lo.id == selectedLOID)].ploLinks].sort((p1, p2) => p1.title.localeCompare(p2.title))
+              .map((plo) => 
+                <li key={plo.id}>
+                  <span>{plo.title}</span><br/>
+                  <span>{plo.description}</span>&nbsp;
+                  <span className="cursor-pointer text-red-600" onClick={() => deleteLinkedPLO(plo.id)}>&#9747;</span>
+                </li>
+              )
+            }
+            {los[los.findIndex((lo) => lo.id == selectedLOID)].ploLinks.length === 0 && <span>No linked PLOs</span>}
+            </ul>
+          </div>  
+        </div>}
+      </div>
+    </div>
+  </>);
+};
+
+function CreateLOLink({ loID, ploGroupID, callback }: { loID: string, ploGroupID: string, callback: () => any }) {
+  if (ploGroupID === '') {
+    return <p></p>;
+  }
+  const GET_PLOS = gql`
+    query PLOs($ploGroupID: ID!) {
+      plos(ploGroupID: $ploGroupID) {
+        id
+        title
+        description
+        ploGroupID
+      }
+    }
+  `;
+  const CREATE_LOLINK = gql`
+    mutation CreateLOLink($loID: ID!, $ploID: ID!) {
+      createLOLink(loID: $loID, ploID: $ploID) {
+        loID
+        ploID
+      }
+    }
+  `;
+  const { data, loading }  = useQuery<{plos: PLOModel[]}, {ploGroupID: string}>(GET_PLOS, {variables: {ploGroupID}});
+  const [createLOLink, {loading: submitting}] = useMutation<{createLOLink: CreateLOLinkResponse}, {loID: string, ploID: string}>(CREATE_LOLINK);
+  const { register, handleSubmit, setValue } = useForm<{ploID: string}>({defaultValues: {ploID: ''}});
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit((form) => {
+        if (form.ploID !== '' && !submitting) {
+          createLOLink({
+            variables: {
+              loID,
+              ploID: form.ploID
+            }
+          }).then(() => {
+            setValue('ploID', '');
+            callback();
+          });
+        }
+      })}>
+        <select {...register('ploID')} className="border-4 rounded-md p-1 mx-2 text-sm" defaultValue="">
+          <option disabled value="">--Select PLO--</option>
+          {[...data.plos].sort((p1, p2) => p1.title.localeCompare(p2.title)).map((plo) => (
+            <option value={plo.id} key={plo.id}>
+              {plo.title}
+            </option>
+          ))}
+        </select>
+        <input type="submit" value="add" className="py-2 px-4 bg-green-300 hover:bg-green-500 rounded-lg"/>
+      </form>
+    </div>
+  );
+}
+
+function CreateLOForm({courseID, callback}: {courseID: string, callback: () => any}) {
+  const CREATE_LO = gql`
+    mutation CreateLO($courseID: ID!, $input: CreateLOInput!) {
+      createLO(courseID: $courseID, input: $input) {
+        id
+      }
+    }
+  `;
+  const [createLO, { loading }] = useMutation<{createLO: CreateLOResponse}, {courseID: string, input: CreateLOModel}>(CREATE_LO);
+  const [show, setShow] = useState<boolean>(false);
+  const { register, handleSubmit, reset } = useForm<CreateLOModel>({
+    defaultValues: {
+      title: '',
+      level: 1,
+      description: '',
+    }
+  });
+  const resetForm = () => {
+    reset({title: '', level: 1, description: ''});
+    setShow(false);
+  };
+  const submitForm = (form: CreateLOModel) => {
+    if (form.title !== '' && form.description !== '') {
+      createLO({
+        variables: {
+          courseID,
+          input: form
+        }
+      }).then(() => {
+        resetForm();
+        callback();
+      });
+    }
+  };
+  return (
+    <div>
+      <button onClick={() => setShow(true)}>Create a new LO</button>
+      <Modal show={show} onHide={() => resetForm()}>
+        <form onSubmit={handleSubmit((form) => loading? null: submitForm(form))}>
+          <Modal.Header>
+            <Modal.Title>Create a new LO</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <span>LO Title:</span><br/>
+            <input type="text" {...register('title', {required: true})} className="border-4 rounded-md p-1 mx-2 text-sm"/><br/>
+            <span>Initial LO Level:</span><br/>
+            <span className="border-4 rounded-md p-1 mx-2 text-sm bg-gray-200 inline-block w-1/3">{1}</span><br/>
+            <span>Level Description:</span><br/>
+            <textarea {...register('description', {required: true})} className="border-4 rounded-md p-1 mx-2 text-sm" cols={40} rows={4}/><br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <input type="submit" value="create" className="py-2 px-4 bg-green-300 hover:bg-green-500 rounded-lg"/>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
+function CreateLOLevelForm({loID, initLevel, callback}: {loID: string, initLevel: number, callback: () => any}){
+  const CREATE_LOLEVEL = gql`
+    mutation CreateLOLevel($loID: ID!, $input: CreateLOLevelInput!) {
+      createLOLevel(loID: $loID, input: $input) {
+        id
+      }
+    }
+  `;
+  const [show, setShow] = useState<boolean>(false);
+  const [createLOLevel, {loading: submitting}] = useMutation<{createLOLevel: CreateLOLevelResponse}, {loID: string, input: CreateLOLevelModel}>(CREATE_LOLEVEL);
+  const { register, handleSubmit, setValue } = useForm<CreateLOLevelModel>({
+    defaultValues: {
+      level: initLevel,
+      description: '',
+    }
+  });
+  return (
+    <>
+      <button onClick={() => setShow(true)} className="bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded text-sm">
+        Create a new LO level <span className="text-xl text-green-800">+</span>
+      </button>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <form
+          onSubmit={handleSubmit((form) => {
+            if (form.description === '' || submitting) {
+              return;
+            }
+            createLOLevel({
+              variables: {
+                loID,
+                input: form
+              }
+            }).then(() => {
+              setValue('description', '');
+              setShow(false);
+              callback();
+            });
+          })}
+        >
+          <Modal.Header>
+            <Modal.Title>Create a new LO level</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <span>LO Level:</span><br/>
+            <span className="border-4 rounded-md p-1 mx-2 text-sm bg-gray-200 pointer-events-none inline-block w-1/3" >{initLevel}</span><br/>
+            <span>Description:</span><br/>
+            <textarea {...register('description')} className="border-4 rounded-md p-1 mx-2 text-sm" cols={40} rows={4}></textarea><br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <input type="submit" value="create" className="py-2 px-4 bg-green-300 hover:bg-green-500 rounded-lg"/>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </>
+  );
+}
 
 interface Params extends ParsedUrlQuery {
   id: string;
@@ -53,6 +381,7 @@ export const getStaticProps: GetStaticProps<{course: CourseModel}> = async (cont
         id
         name
         programID
+        ploGroupID
       }
     }
   `;
--- apps/web/pages/course/[id]/quizzes.tsx
+++ apps/web/pages/course/[id]/quizzes.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import client from '../../../apollo-client';
-import { gql } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { CourseStaticPaths } from '../../../utils/staticpaths';
 import ProgramAnchor from '../../../components/ProgramAnchor';
 import ClientOnly from '../../../components/ClientOnly';
 import { ParsedUrlQuery } from 'querystring';
+import { Modal } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import xlsx from 'xlsx';
 
 interface CourseModel {
   id: string;
@@ -16,6 +20,98 @@ interface CourseModel {
   year: number;
   ploGroupID: string;
   programID: string;
+};
+
+interface QuestionUpload { 
+  /**
+   * Excel Header Format
+   */
+  questionTitle: string;
+  maxScore: number;
+  studentID: string;
+  studentScore: number;
+};
+
+interface QuizModel {
+  id: string;
+  name: string;
+  createdAt: number;
+  questions: QuestionModel[];
+};
+
+interface QuestionModel {
+  id: string;
+  title: string;
+  maxScore: number;
+  results: {
+    studentID: string;
+    score: number;
+  }[];
+  loLinks: QuestionLinkModel[];
+};
+
+interface QuestionLinkModel {
+  loID: string;
+  level: number;
+  description: string;
+};
+
+interface LOModel {
+  id: string;
+  title: string;
+  levels: {
+    level: number;
+    description: string;
+  }[];
+  ploLinks: {
+    id: string;
+    title: string;
+    description: string;
+    ploGroupID: string;
+  }[];
+};
+
+interface CreateQuestionLinkModel {
+  questionID: string;
+  loID: string;
+  level: number;
+};
+
+interface CreateQuestionLinkResponse {
+  questionID: string;
+  loID: string;
+};
+
+interface CreateQuizModel {
+  name: string;
+  createdAt: Date;
+  questions: CreateQuestionModel[];
+};
+
+interface CreateQuizResponse {
+  id: string;
+};
+
+interface CreateQuestionModel {
+  title: string;
+  maxScore: number;
+  results: CreateQuestionResultModel[];
+};
+
+interface CreateQuestionResultModel {
+  studentID: string;
+  score: number;
+};
+
+interface DeleteQuestionLinkModel {
+  questionID: string;
+  loID: string;
+  level: number;
+};
+
+interface DeleteQuestionLinkResponse {
+  questionID: string;
+  loID: string;
 };
 
 export default function Index({course}: {course: CourseModel}) {
@@ -36,8 +132,281 @@ export default function Index({course}: {course: CourseModel}) {
       {' '}&#12297;{' '}
       <span>Quizzes</span>
     </p>
-    {JSON.stringify(course)}
+    <ClientOnly>
+      <Quiz courseID={course.id}/>
+    </ClientOnly>
   </div>);
+};
+
+function Quiz({courseID}: {courseID: string}) {
+  const GET_LOS = gql`
+    query LOs($courseID: ID!) {
+      los(courseID: $courseID) {
+        id
+        title
+        levels {
+          level
+          description
+        }
+        ploLinks {
+          id
+          title
+          description
+          ploGroupID
+        }
+      }
+    }
+  `;
+  const GET_QUIZZES = gql`
+    query Quizzes($courseID: ID!) {
+      quizzes(courseID: $courseID) {
+        id
+        name
+        createdAt
+        questions {
+          id
+          title
+          maxScore
+          loLinks {
+            loID
+            level
+            description
+          }
+        }
+      }
+    }
+  `;
+  const DELETE_QUESTIONLINK = gql`
+    mutation DeleteQuestionLink($input: DeleteQuestionLinkInput!) {
+      deleteQuestionLink(input: $input) {
+        questionID
+        loID
+      }
+    }
+  `;
+  const los = useQuery<{los: LOModel[]}, {courseID: string}>(GET_LOS, {variables: {courseID}});
+  const [selectedQuestionID, setSelectedQuestionID] = useState<string>('');
+  const { data, loading, refetch } = useQuery<{quizzes: QuizModel[]}, {courseID: string}>(GET_QUIZZES, { variables: { courseID } });
+  const [deleteQuestionLink, {loading: submitting}] = useMutation<{deleteQuestionLink: DeleteQuestionLinkResponse}, {input: DeleteQuestionLinkModel}>(DELETE_QUESTIONLINK);
+  let quizzes: QuizModel[] =  [];
+  if (data) {
+    quizzes = [...data.quizzes];
+  }
+  let questionLinks: QuestionLinkModel[] = [];
+  if (selectedQuestionID !== '' && data) {
+    questionLinks = quizzes.filter((quiz) => quiz.questions.findIndex((question) => question.id === selectedQuestionID) !== -1)[0]
+      .questions.filter((question) => question.id === selectedQuestionID)[0].loLinks;
+  }
+  const removeQuestionLink = (loID: string, level: number) => {
+    if (submitting) {
+      return;
+    }
+    deleteQuestionLink({
+      variables: {
+        input: {
+          questionID: selectedQuestionID,
+          loID,
+          level
+        }
+      }
+    }).then(() => refetch());
+  };
+  return (<>
+    <CreateQuizForm courseID={courseID} callback={refetch}/>
+    <div className="grid grid-cols-2 gap-x gap-x-6 mt-2">
+      <div className="flex flex-column space-y-2">
+        {loading && <p>Loading...</p>}
+        {quizzes.sort((q1, q2) => q1.name.localeCompare(q2.name)).map((quiz) => (
+        <div key={quiz.id} className="rounded shadow-lg p-3">
+          <div className="flex justify-between items-center">
+            <span className="font-bold">{quiz.name}</span>
+          </div>
+          <ul>
+          {
+            [...quiz.questions].sort((q1, q2) => q1.title.localeCompare(q2.title)).map((question, index) => (
+              <li key={question.id}>
+                Q{index + 1}) {question.title} (max score: {question.maxScore})
+                <div className="flex flex-row-reverse space-x-2">
+                  <button
+                    onClick={() => {setSelectedQuestionID(question.id)}}
+                    className={`bg-gray-200 hover:bg-gray-400 py-1 px-2 rounded text-sm ${selectedQuestionID===question.id?'bg-blue-400 hover:bg-blue-300':''}`}>
+                    Manage LOs <span className="text-xl text-green-800">&#9874;</span>
+                  </button>
+                </div>
+              </li>
+            ))
+          }
+          </ul>
+          <br/>
+        </div>
+        ))}
+      </div>
+      <div>
+        {selectedQuestionID !== '' && 
+        <div className="flex flex-column divide-y-4">
+          <CreateQuestionLinkForm los={[...los.data.los]} questionID={selectedQuestionID} callback={refetch}/>
+          <div className="pt-3">
+            <span>Linked LOs: </span><br/>
+            <ul>
+            {
+              [...questionLinks].sort((l1, l2) => l1.description.localeCompare(l2.description)).map((lo) => (
+                <li key={`${lo.loID}-${lo.level}`}>
+                  {lo.description}&nbsp;
+                  <span className="cursor-pointer text-red-600" onClick={() => removeQuestionLink(lo.loID, lo.level)}>&#9747;</span>
+                </li>
+              ))
+            }
+            {questionLinks.length === 0 && <span>No linked LOs</span>}
+            </ul>
+          </div>  
+        </div>}
+      </div>
+    </div>
+  </>);
+};
+
+function CreateQuizForm({courseID, callback}: {courseID: string, callback: () => any}) {
+  const CREATE_QUIZ = gql`
+    mutation CreateQuiz($courseID: ID!, $input: CreateQuizInput!) {
+      createQuiz(courseID: $courseID, input: $input) {
+        id
+      }
+    }
+  `;
+  const [createQuiz, {loading: submitting}] = useMutation<{createQuiz: CreateQuizResponse}, {courseID: string, input: CreateQuizModel}>(CREATE_QUIZ);
+  const [show, setShow] = useState<boolean>(false);
+  const { register, handleSubmit, setValue } = useForm<{ name: string }>();
+  const [excelFile, setExcelFile] = useState<QuestionUpload[]>([]);
+  const excelJSON = (file) => {
+    let reader = new FileReader();
+    reader.onload = function(e) {
+      let data = e.target.result;
+      let workbook = xlsx.read(data, {type: 'binary'});
+      setExcelFile(xlsx.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]));
+    }
+    reader.onerror = console.log;
+    reader.readAsBinaryString(file);
+  };
+  return (
+    <div>
+      <button onClick={() => setShow(true)}>Create a new quiz.</button>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <form
+          onSubmit={handleSubmit((form) => {
+            if (form.name === '' || excelFile.length === 0 || submitting) {
+              return
+            }
+            let questions = new Map<string, CreateQuestionModel>();
+            for (let i = 0; i < excelFile.length; ++i) {
+              let question: CreateQuestionModel = {
+                title: '',
+                maxScore: 0,
+                results: []
+              };
+              if (questions.has(excelFile[i].questionTitle)) {
+                question = questions.get(excelFile[i].questionTitle);
+              }
+              question.title = excelFile[i].questionTitle;
+              question.maxScore = excelFile[i].maxScore;
+              question.results = [...question.results, {
+                studentID: `${excelFile[i].studentID}`,
+                score: excelFile[i].studentScore
+              }];
+              questions.set(excelFile[i].questionTitle, question);
+            }
+            createQuiz({
+              variables: {
+                courseID,
+                input: {
+                  name: form.name,
+                  createdAt: new Date(),
+                  questions: [...questions.values()]
+                }
+              }
+            }).then(() => {
+              setValue('name', '');
+              setExcelFile([]);
+              setShow(false);
+              callback();
+            });
+          })}
+        >
+          <Modal.Header>
+            <Modal.Title>Create a new quiz</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <span>Quiz name:</span><br/>
+            <input type="text" {...register('name')} placeholder="quiz name" className="border-4 rounded-md p-1 mx-2 text-sm"/><br/>
+            <span>Upload quiz result:</span><br/>
+            <input type="file" onChange={e => excelJSON(e.target.files[0])} className="p-1 mx-2 text-sm"/><br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <input type="submit" value="create" className="py-2 px-4 bg-green-300 hover:bg-green-500 rounded-lg"/>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+const CreateQuestionLinkForm: React.FC<{los: LOModel[], questionID: string, callback: () => any}> = ({los, questionID, callback}) => {
+  const CREATE_QUESTIONLINK = gql`
+    mutation CreateQuestionLink($input: CreateQuestionLinkInput!) {
+      createQuestionLink(input: $input) {
+        questionID
+        loID
+      }
+    }
+  `;
+  const [createQuestionLink, { loading: submitting }] = useMutation<{createQuestionLink: CreateQuestionLinkResponse}, {input: CreateQuestionLinkModel}>(CREATE_QUESTIONLINK);
+  const [selectedLOID, setSelectedLOID] = useState<string>('');
+  const { register, handleSubmit, setValue } = useForm<CreateQuestionLinkModel>({defaultValues: {loID: '', level: 0}});
+  const resetForm = () => {
+    setValue('loID', '');
+    setValue('level', 0);
+  };
+  const submitForm = (form: CreateQuestionLinkModel) => {
+    if (form.loID === '' || form.level === 0 || submitting) {
+      return;
+    }
+    createQuestionLink({
+      variables: {
+        input: {
+          ...form,
+          questionID
+        },
+      }
+    }).then(() => {
+      resetForm();
+      setSelectedLOID('');
+      callback();
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit((form) => submitForm(form))}>
+      <span>Select LO:</span><br/>
+      <select {...register('loID')} className="border-4 rounded-md p-1 mx-2 text-sm w-2/4" defaultValue="" onChange={e => {setSelectedLOID(e.target.value);setValue('level', 0);}}>
+        <option disabled value="">--Select LO--</option>
+        {los.sort((l1, l2) => l1.title.localeCompare(l2.title)).map((lo) => (
+          <option value={lo.id} key={lo.id}>
+            {lo.title}
+          </option>
+        ))}
+      </select><br/>
+      {selectedLOID !== '' && <div>
+        <span>Select Level:</span><br/>
+        <select {...register('level')} className="border-4 rounded-md p-1 mx-2 text-sm w-2/4" defaultValue={0}>
+          <option disabled value={0}>--Select Level--</option>
+          {los[los.findIndex((lo) => lo.id == selectedLOID)] && los[los.findIndex((lo) => lo.id == selectedLOID)].levels.map((level) => (
+            <option value={level.level} key={level.description}>
+              {level.description}
+            </option>
+          ))}
+        </select>
+      </div>}<br/>
+      <input type="submit" value="add" className="py-2 px-4 bg-green-300 hover:bg-green-500 rounded-lg"/>
+    </form>
+  );
 };
 
 interface Params extends ParsedUrlQuery {
